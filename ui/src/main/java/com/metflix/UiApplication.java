ef51ea6c6e1e7b1f9379375e5392fceeff78b713
ryo-murai <murai.ry@gmail.com>
2019-01-21 14:48:21 +0900

    fix enabling circuit breaker for ui app

--- ui/src/main/java/com/metflix/UiApplication.java
+++ ui/src/main/java/com/metflix/UiApplication.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
@@ -38,6 +39,7 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableCircuitBreaker
 public class UiApplication extends WebSecurityConfigurerAdapter {
 
 	public static void main(String[] args) {
